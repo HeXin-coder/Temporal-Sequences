31d539938ea84499026852e397d57915d29a97f1
HeXin <77604543+HeXin-coder@users.noreply.github.com>
2024-03-21 10:19:36 +0800

    Create README.m

---
+++ tqyb/README.m
@@ -0,0 +1,5 @@
+- this folder is used to reserve the code for lecture "7C150004 Space weather forecast"
+
+- `tqyb_onestep.ipynb` and `tqyb_multistep.ipynb` are based on Markov Models to realize the weather forecast.
+- `Markov.m` is the compiled version of `tqyb_onestep.ipynb` using matlab.
+- `model.mat` is the model which is trained in `Markov.m`.
